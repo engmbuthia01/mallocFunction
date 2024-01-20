f3a00dc9b13596e5d99c347dc3f9cc23afeef16b
engmbuthia01 <kevinmbuthia2254@gmail.com>
2024-01-20 01:05:51 -0500

    automatic memory allocation

---
+++ memory_allocation/automatic_allocation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This is the main function
+ * @a: integer value
+ * @s: string value
+ *
+ */
+int main()
+{
+    int s;
+    s = "engmbuthia01";
+
+    printf("%s\n", s);
+    return(0);
+}
